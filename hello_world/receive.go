811774b589b04dd333435c2b24808119ac73566e
maciekole <24421260+maciekole@users.noreply.github.com>
2023-11-05 00:51:50 +0100

    hello world in rabbit

---
+++ hello_world/receive.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare("hello", false, false, false, false, nil)
+	failOnError(err, "Failed to declare queue")
+
+	msgs, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+	failOnError(err, "Failed to register Consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf("[*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
