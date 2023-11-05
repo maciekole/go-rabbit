80b3b0484b1078d80b716ff1717f940826e34d43
maciekole <24421260+maciekole@users.noreply.github.com>
2023-11-05 21:52:01 +0100

    logs routing

---
+++ routing/emit_log_direct.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func bodyFrom(args []string) string {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "hello"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	return s
+}
+
+func severityFrom(args []string) string {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "info"
+	} else {
+		s = os.Args[1]
+	}
+	return s
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare("logs_direct", "direct", true, false, false, false, nil)
+	failOnError(err, "Failed to decalre exchange")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := bodyFrom(os.Args)
+	err = ch.PublishWithContext(ctx, "logs_direct", severityFrom(os.Args), false, false, amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte(body),
+	})
+	failOnError(err, "Failed to publish a message")
+	log.Printf("[X] Sent %s", body)
+}
